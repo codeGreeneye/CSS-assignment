5d2f94f855ede28c43ded5fea475fba46c711090
Himani Sharma <149160134+codeGreeneye@users.noreply.github.com>
2023-11-24 16:19:53 +0000

    javascript addded

---
+++ script.js
@@ -0,0 +1,17 @@
+let fruit;
+fruit = 18;
+//fruit = fruit + 1;
+fruit *= 2;
+fruit += 1;
+
+const age = 21;
+const myName = "Himani";
+const vegan = "true";
+let favSubject = undefined;
+let isScholar = null;
+//console.log(typeof age, typeof myName);
+//console.log(typeof vegan);
+//console.log(favSubject);
+console.log(typeof isScholar);
+const friends = ["a", "b", "c", "d"];
+console.log(friends[3]);
